55e1b63e3b6af952d327ed285cb7be3df7836952
Jens Kleinjung <jens@jkleinj.eu>
2019-07-10 18:30:35 +0100

    added GPL3 notice

--- sa_encode.R
+++ sa_encode.R
@@ -1,6 +1,22 @@
 #===============================================================================
-# AlloHubMat
-#' sa_encode.R
+# AlloHubMat::sa_encode.R
+# Encode a protein structure as structural string
+# (C) 2019 Franca Fraternali, Jens Kleinjung, Jamie Macpherson
+#
+# This file is part of AlloHubMat.
+#
+#    AlloHubMat is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
 #'
 #' Encode a protein structure with the M32K25 structural alphabet.
 #'
